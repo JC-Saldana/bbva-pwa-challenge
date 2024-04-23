3ea655b9f538d81ad24996ce2ab9899a53004c80
Carlos Saldaña <jcsaldanah@gmail.com>
2024-04-23 20:09:49 +0200

    feat: editorconfig

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		node: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: ['xo'],
+	parser: '@typescript-eslint/parser',
+	ignorePatterns: ['**/dist/**/*.js'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'object-curly-spacing': ['Error', 'always'],
+		'operator-linebreak': ['error', 'after'],
+	},
+};
