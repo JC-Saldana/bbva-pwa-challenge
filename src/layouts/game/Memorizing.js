42d21d436f67c639ba26ae1aa16cfee2fc31d819
Carlos Saldaña <jcsaldanah@gmail.com>
2024-04-24 09:11:46 +0200

    fix: editor config styling

--- src/layouts/game/Memorizing.js
+++ src/layouts/game/Memorizing.js
@@ -5,15 +5,15 @@ import { gameStatuses } from "../../constants";
 import { useMemo } from "react";
 
 export default function Memorizing({ seconds, setGameStatus, randomNumbers, gameResult, gameStatus }) {
-    const MemoizedCustomProgress = useMemo(() => (
-        <CustomProgress
-            seconds={seconds}
-            onTimeFinish={() => setGameStatus(gameStatuses.playing)} />,
-        [seconds, setGameStatus])
-    );
-    return <>
-        <Typography variant="h4" component="h4">Memorize the cards</Typography>
-        {MemoizedCustomProgress}
-        <Cards randomNumbers={randomNumbers} gameResult={gameResult} gameStatus={gameStatus} />
-    </>
+  const MemoizedCustomProgress = useMemo(() => (
+    <CustomProgress
+      seconds={seconds}
+      onTimeFinish={() => setGameStatus(gameStatuses.playing)} />,
+    [seconds, setGameStatus])
+  );
+  return <>
+    <Typography variant="h4" component="h4">Memorize the cards</Typography>
+    {MemoizedCustomProgress}
+    <Cards randomNumbers={randomNumbers} gameResult={gameResult} gameStatus={gameStatus} />
+  </>
 }
