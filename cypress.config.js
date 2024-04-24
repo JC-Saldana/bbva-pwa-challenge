cb5e7e61e9846cdf7d46bcc3d88076d2742e56cd
Carlos Saldaña <jcsaldanah@gmail.com>
2024-04-24 13:35:26 +0200

    fix: cypress config to work on localhost

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  baseUrl: "http://localhost:3000", // Set the baseUrl to your local development server
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000"
   },
 });
