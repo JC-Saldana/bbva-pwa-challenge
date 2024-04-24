e3fc260611864e5f8cb996d66acd76e9c33049b2
Carlos Saldaña <jcsaldanah@gmail.com>
2024-04-24 13:33:11 +0200

    fix: run cypress on local

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  baseUrl: "http://localhost:3000", // Set the baseUrl to your local development server
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/game.cy.js
+++ cypress/e2e/game.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://bbva-pwa-challenge.netlify.app/')
+    cy.visit('http://localhost:3000')
     cy.get('input[type="text"]').type('User');
     cy.get('input[type="text"]').should('have.value', 'User');
     cy.contains('Login').click()
